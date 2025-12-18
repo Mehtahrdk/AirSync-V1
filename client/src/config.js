b7719b33b3afded2f7201c4f5990e283711436c9
Hardik Mehta <mhardik2004@gmail.com>
2025-12-18 18:49:50 +0530

    Point frontend to Live Backend

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // If we are in production (live), use the live backend URL.
 // If we are local, use localhost.
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
+export const API_BASE_URL ="https://airsync-backend.onrender.com/api/v1";
